2a6a7706841c47c92c7f3e74e6dcb1243700679b
lewisyng <lewisyoung1306@yahoo.de>
2022-04-24 15:22:27 +0200

    env variable fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    env: {
+        OPEN_WEATHER_MAP_API_KEY: '8cb808e9039d132fd4ef16673a3d3def',
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
